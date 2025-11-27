cc9dd768dea2a30aa188882e64a404dee1d90cb9
carluandraster <andres.efstratiadis@gmail.com>
2025-11-27 15:21:42 -0300

    Update test_eliminar_innecesarios.py

--- test/test_eliminar_innecesarios.py
+++ test/test_eliminar_innecesarios.py
@@ -1,2 +1,12 @@
 import src.eliminar_innecesarios as ei
+import unittest
 
+
+class EliminarInnecesariosTest(unittest.TestCase):
+    def testInitCodeAnalyzer(self):
+        analyzer = ei.CodeAnalyzer()
+        self.assertEqual(analyzer.functions, set())
+        self.assertEqual(analyzer.classes, set())
+        self.assertEqual(analyzer.calls, {})
+        self.assertEqual(analyzer.used, set())
+        self.assertIsNone(analyzer.current_context)
