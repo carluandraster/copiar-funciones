135c6a22371f245f4853dc749f6243d9c40e3560
carluandraster <andres.efstratiadis@gmail.com>
2025-11-27 16:35:22 -0300

    Remover código

--- test/experimentos.py
+++
@@ -1,7 +0,0 @@
-import lib.MatrizFactory as mf
-import lib.Unidad4 as u4
-
-if __name__ == "__main__":
-    matriz_de_ceros = mf.ceros(3,3)
-    print(matriz_de_ceros)
-    print("Rendimiento: ", u4.get_rendimiento_y_redundancia([0.5, 0.5], ["0", "1"])[0])
